50c6bb3ac69f9d2833f6adf530912f30eb0d0419
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-26 16:46:21 +0500

    Add abstract Aircraft class

--- src/part2/Aircraft.java
+++ src/part2/Aircraft.java
@@ -1,4 +1,25 @@
 package part2;
 
-public class Aircraft {
+public abstract class Aircraft {
+    protected String id;
+    protected int fuelLevel;
+
+    public Aircraft(String id, int fuelLevel) {
+        this.id = id;
+        this.fuelLevel = fuelLevel;
+    }
+
+    public abstract void receive(String msg);
+
+    public void send(String msg, TowerMediator tower) {
+        tower.broadcast(msg, this);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
 }
