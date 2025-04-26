d20694fadada491989b4ffc2dab3c9f8afe49ab1
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-26 15:04:51 +0500

    Add Season class implementing Iterable

---
+++ src/part1/Season.java
@@ -0,0 +1,22 @@
+package part1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    @Override
+    public Iterator<Episode> iterator() {
+        return new SeasonIterator(this);
+    }
+}
