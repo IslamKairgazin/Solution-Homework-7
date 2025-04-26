bb4458ba2f4ef5c4c74b256ecc61e73d969ca7b9
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-26 17:09:06 +0500

    Add Helicopter class extending Aircraft

--- src/part2/Helicopter.java
+++ src/part2/Helicopter.java
@@ -1,4 +1,12 @@
 package part2;
 
-public class Helicopter {
+public class Helicopter extends Aircraft {
+    public Helicopter(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("Helicopter " + id + " received: " + msg);
+    }
 }
