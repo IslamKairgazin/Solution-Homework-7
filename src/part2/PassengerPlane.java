e72a047294bbae2772d4abbee55e9ca9b0c49b0b
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-04-26 17:10:24 +0500

    Add PassengerPlane class

--- src/part2/PassengerPlane.java
+++ src/part2/PassengerPlane.java
@@ -1,4 +1,12 @@
 package part2;
 
-public class PassengerPlane {
+public class PassengerPlane extends Aircraft {
+    public PassengerPlane(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("PassengerPlane " + id + " received: " + msg);
+    }
 }
